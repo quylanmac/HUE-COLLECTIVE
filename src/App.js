517200b1968b42244a72f9511b01c87d6be98701
Quylan Mac <qmac@UCSD.EDU>
2020-04-20 19:02:31 -0700

    Some cleaning up

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import './App.css';
-//import 'bootstrap/dist/css/bootstrap.min.css';
-import {Container, Row, Col, Navbar} from 'react-bootstrap';
-import logo from './img/HUE.png';
-import {HUEBar} from './Components/HUEBar';
-import {ColourWheel} from './Components/ColourWheel';
-import {Canvas} from './Components/Canvas';
-import {Test} from './Components/Test';
+import { mainPage } from './Components/mainPage';
 function App() {
   return (
-    <Test></Test>
+    <mainPage></mainPage>
   );
 }
 
