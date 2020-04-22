911238e3235d4fff712d658940b877c0c25980f0
Quylan Mac <qmac@UCSD.EDU>
2020-04-21 18:27:44 -0700

    animations and cleanup

--- src/Components/ColourWheel.jsx
+++ src/Components/ColourWheel.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from '../img/logo.png';
+import logo from '../img/huecropped2.png';
 import '../App.css';
 import cursor2 from '../img/cursor.svg';
 export class ColourWheel extends React.Component {
@@ -8,34 +8,35 @@ export class ColourWheel extends React.Component {
 
     this.state = {
       dragging: false,
-      renderChild:false
+      renderChild:false     //default is false
     };
 
     this.options = {
-      width: 800,
-      height:569,
+      width: 1024,
+      height:1024,
       img: this.props.img 
     };
     this.interval=null;
   }
 
   componentDidMount() {
-    this.interval=setTimeout(()=>this.setState({renderChild:true}),10000);
+    this.interval=setTimeout(()=>this.setState({renderChild:true}),6500);
     this.addImageInCanvas();
   }
   componentWillUnmount() {
     clearInterval(this.interval);
 }
   render() {
-    var visibilityState=this.state.renderChild ? "visible" : "visible";
+   // var visibilityState=this.state.renderChild ? "hidden" : "visible"; //dont think is needed anymore 
+   var visibilityState="visible";
     return (
       <>
       <canvas
        // onMouseDown={() => this.setState({ dragging: true })}
         onMouseMove={e => this.selectColor(e)}
-        //onMouseUp={() => this.setState({ dragging: false })}
+       // onMouseUp={() => this.setState({ dragging: false })}
         className="custom-cur"
-        style={{visibility: visibilityState, cursor: `url(${cursor2}),auto;`}}
+        style={{visibility: visibilityState, width:"100%"}}
         ref="canvas"
         width={this.options.width}
         height={this.options.height}
@@ -57,7 +58,7 @@ export class ColourWheel extends React.Component {
 
   selectColor(e) {
       const ctx = this.refs.canvas.getContext("2d");
-  //  if (!this.state.dragging) return;
+   //if (!this.state.dragging) return;
     let { offsetX, offsetY } = e.nativeEvent;
     let { data } = ctx.getImageData(offsetX, offsetY, 1, 1);
     let color = `rgb(${data[0]}, ${data[1]}, ${data[2]})`;
--- src/Components/Test.jsx
+++ src/Components/Test.jsx
@@ -3,13 +3,14 @@ import myCss from '../App.css';
 import {Container, Row, Col, Button} from 'react-bootstrap';
 import {ColourWheel} from './ColourWheel';
 import {Loader} from './Loader';
+import '../animation.css';
 export class Test extends React.Component {
     constructor(props) {
         super(props);
         
         this.state={
             background:'#110F11',
-            vis:false
+            vis:false  //should be false
         };
         this.interval=null;
     }
@@ -18,7 +19,7 @@ export class Test extends React.Component {
         this.setState({background:color.hex});
     };
     componentDidMount() {
-        this.interval=setTimeout(()=>this.setState({vis:true}),10000);
+        this.interval=setTimeout(()=>this.setState({vis:true}),5000);
     }
     componentWillUnmount() {
         clearInterval(this.interval);
@@ -29,19 +30,16 @@ export class Test extends React.Component {
             <div style={{backgroundColor:"#121212",minHeight:"100vh"}}>
             <Container className="d-flex flex-column justify-content-center">
               <Row>
-                <Col className="col-auto mx-auto">      
-                 <Loader></Loader>
-                </Col>
-              </Row>
-              <Row>
-                  
                 <Col className="col-auto mx-auto">
-                    {this.state.vis ?    <Button href="index.html" className="mybutton"><ColourWheel
-                     callback={color=>this.setState({background:color})}></ColourWheel></Button> : null}
+                    {/*Overlay classes are used to position the loader right on top of the ColourWheel component*/}  
+                    <div className="overlay" style={{marginTop:"-10.5%"}}>   
+                        <div className="overlay-1"><Loader></Loader></div>
+                        {/*This checks for the vis bool to display the colour wheel component which is set after a timed delay*/}
+                        {this.state.vis ? <div className="overlay-2 animated fadeIn delay2"><Button href="index.html" className="mybutton"><ColourWheel callback={color=>this.setState({background:color})}></ColourWheel></Button></div> : null}
+                     </div>
                 </Col>
-            
               </Row>
-                <Row>
+                <Row style={{marginTop:"-30%"}}>
                     <Col className="col-auto mx-auto">
                         <a href="index.html" style={{color:this.state.background}}><p>Enter site</p></a>
                     </Col>
