caa9315b3248b72de048146b5deeb861b2a7418d
Quylan Mac <qmac@UCSD.EDU>
2020-04-26 02:35:25 -0700

    card update again

--- src/Components/Test.jsx
+++ src/Components/Test.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import myCss from '../App.css';
 import {Container, Row, Col, Card, Button, Modal} from 'react-bootstrap';
+import '../App.css';
 import {ColourWheel} from './ColourWheel';
 import {Loader} from './Loader';
 import '../animation.css';
-import {Redirect, Link} from "react-router-dom";
-import {TeamCard} from './TeamCard';
 import data from '../data.json';
-
 export class Test extends React.Component {
     constructor(props, context) {
         super(props, context);
@@ -68,25 +65,95 @@ export class Test extends React.Component {
             <h1>{data.name}</h1>
           </Button>
               <Modal
-                show={this.state.show === data.id} onHide={this.handleClose}
-              >
-                <Card className="secondCardHead" style={{backgroundColor:data.color1}}>{data.name}
-                <div style={{height:"450px"}}>
-                <Card.Img className = "myCard" src={require(`../Members/Sketch/${data.image}.png`)} style={{}}/>
-                </div>
-                    <Card.Title className="secondCard" style={{backgroundColor:data.color2}}>{data.bio}</Card.Title>
-                    <Card.Subtitle style={{backgroundColor:data.color3}}>{data.role}</Card.Subtitle>
+              dialogClassName="modal-90w"
+                show={this.state.show === data.id} onHide={this.handleClose} 
+              > 
+             
+                <Card className="myCard">
+                    
+                <div className="TOPCARD mx-auto" style={{backgroundColor:data.color1}}>
+
+                    <Row>
+                        <Col className="mx-auto">
+                            <Card.Title className="cardName">{data.name}</Card.Title>
+                        </Col>
+                    </Row>
+                            {/*<p className="sidebars" style={{transform:"rotate(-90deg)", textAlign:"center"}}>{data.role}</p>*/}
+
+                    <Row>
+
+                        <Col className="col-xs-6 col-md-3 d-flex justify-content-center align-items-center">
+                            <h2 className="text-align sidebar1">{data.role}</h2>
+                        </Col>
+
+                        <Col className="col-xs-12 col-md-6">
+                            <Card.Img className="img-fluid d-block" src={require(`../Members/Sketch/${data.image}.png`)}/>
+                        </Col>
+
+                        <Col className="col-xs-6 col-md-3 d-flex justify-content-center align-items-center">
+                            <p className="text-align sidebar2">{data.major}</p>
+                        </Col>
+
+                    </Row>
+                           
+                    <Row>
+                        <Col className="mt-auto">
+                            <p className="color" style={{textAlign:"right"}}>{data.color1}</p>
+                        </Col>
+                    </Row>
+            </div>
+                        
+            <div className="MIDCARD" style={{backgroundColor:data.color2}}>
+                
+                       <Row>
+                           <Col>
+                        <Card.Subtitle className="secondCard">
+                            <div style={{marginTop:"1.5rem", marginLeft:"1.5rem", marginRight:"1.5rem"}}>
+                                <p className="memberBio">{data.bio}</p>
+                            </div>
+                        </Card.Subtitle>
+                        </Col>
+
+                        </Row>
+
+                        <Row>
+                        <Col className="mt-auto">
+                                <p className="color" style={{textAlign:"right"}}>{data.color2}</p>
+                            </Col>
+                        </Row>
+            </div>
+
+            <div className="BOTCARD" style={{backgroundColor:data.color3}}>
+                <Card.Subtitle>
+                        <Row>
+                            <Col>
+                            <div style={{ marginLeft:"30px", marginRight:"30px", paddingTop:"5%"}}>
+                                <img src={require(`../img/linkedIN.png`)} ></img>
+                                <img src={require(`../img/instagram.png`)} style={{ paddingLeft:"1.5rem"}}></img>
+                                <img src={require(`../img/email.png`)} style={{paddingLeft:"1.5rem"}}></img>
+                            </div>
+                            </Col>
+                        </Row>
+                        </Card.Subtitle>
+
+                <Row>
+                    <Col className="mt-auto">
+                        <p className="color" style={{textAlign:"right"}}>{data.color3}</p>
+                    </Col>
+                </Row>                       
+            </div>   
+
                 </Card>
+              
               </Modal>
               </div>
           )
         });
       }
     render() {    
-
         return (
             <div style={{backgroundColor:"#121212",minHeight:"100vh"}}>
-            <Container className="d-flex flex-column justify-content-center">
+            <Container className="d-flex flex-column justify-content-center container-fluid">
                  {/*The gif loads with a fade and then transitions into the still image. When image is clicked, color is returned and mainPage is displayed*/}
                     {/*Overlay classes are used to position the loader right on top of the ColourWheel component*/}
                 {this.state.LoaderDiv ?  
@@ -121,12 +188,12 @@ export class Test extends React.Component {
                         </Col>
                     </Row>
                     {/*This takes a JSON file and creates cards */}
-                    <Row className="row-cols-1">
+                    <Row>
                         {data.map( data=> (
                             
-                            <Col xs={3} className="mb-5" key={`${data.id}`}>
-                                <Card className="myCard h-100 shadow-sm bg-white" style={{width:"251px"}} onClick={() => this.handleShow(data.id)}>
-                                    <Card.Img className = "myCard" src={require(`../Members/Picture/${data.image}.png`)} style={{width:"251px", height:"275px"}}/>
+                            <Col xs={4} sm={6} md={3} className="mb-5" key={`${data.id}`}>
+                                <Card className="myCard h-100 shadow-sm bg-white" style={{margin:"auto"}} onClick={() => this.handleShow(data.id)}>
+                                    <Card.Img className = "myCard" src={require(`../Members/Picture/${data.image}.png`)}/>
                                     <Card.Body className="d-flex flex-column">
                                     <div className="d-flex mb-2 justify-content-between">
                                         <Card.Title className="mb-0 cardFont">{data.name}</Card.Title>
@@ -137,7 +204,7 @@ export class Test extends React.Component {
                             </Col>
                             
                         ))}
-                    </Row> 
+                    </Row>
                 </div>
               : null}
               {this.renderModals()}
