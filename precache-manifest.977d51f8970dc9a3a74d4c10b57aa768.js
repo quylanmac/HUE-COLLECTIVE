5aed00b98400a2f65ba907578250845d71b59990
Quylan Mac <qmac@UCSD.EDU>
2020-05-01 12:18:44 -0700

    Updates

---
+++ precache-manifest.977d51f8970dc9a3a74d4c10b57aa768.js
@@ -0,0 +1,454 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57b632c8943467320f26de4669f4d368",
+    "url": "/hue-collective/index.html"
+  },
+  {
+    "revision": "e93553fcf9db1c5b0195",
+    "url": "/hue-collective/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "d77b91c6efee7429a0f5",
+    "url": "/hue-collective/static/css/main.252814c1.chunk.css"
+  },
+  {
+    "revision": "e93553fcf9db1c5b0195",
+    "url": "/hue-collective/static/js/2.1aaa34b0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/hue-collective/static/js/2.1aaa34b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d77b91c6efee7429a0f5",
+    "url": "/hue-collective/static/js/main.362ccd46.chunk.js"
+  },
+  {
+    "revision": "029f13019f195a8dd8c8",
+    "url": "/hue-collective/static/js/runtime-main.32429ade.js"
+  },
+  {
+    "revision": "aa15b22f6126f2a150f9ff3b14a42b19",
+    "url": "/hue-collective/static/media/Cubano-Regular.aa15b22f.otf"
+  },
+  {
+    "revision": "0facaae97183b8fede52099930aefd8d",
+    "url": "/hue-collective/static/media/HelveticaNeue Light.0facaae9.ttf"
+  },
+  {
+    "revision": "ccad04d46768981ff847f22e8a53b5b3",
+    "url": "/hue-collective/static/media/HelveticaNeue.ccad04d4.ttf"
+  },
+  {
+    "revision": "b8edca3e45f1f16bc6e20464bd8f2fff",
+    "url": "/hue-collective/static/media/HelveticaNeueBd.b8edca3e.ttf"
+  },
+  {
+    "revision": "99a8ec3f3a0b9f1f636e2dbc8f7928ed",
+    "url": "/hue-collective/static/media/Vector.99a8ec3f.png"
+  },
+  {
+    "revision": "7fc445e3f04e8ec76a1bd56f5f31f895",
+    "url": "/hue-collective/static/media/adam.7fc445e3.png"
+  },
+  {
+    "revision": "8270bb38c57a2451b1670e29f0a9dbf1",
+    "url": "/hue-collective/static/media/adam.8270bb38.png"
+  },
+  {
+    "revision": "89f0c83a89948319a4065fc61c096e1e",
+    "url": "/hue-collective/static/media/adam.89f0c83a.png"
+  },
+  {
+    "revision": "ba4b88c8bb50fc8f15fb9a7c26658697",
+    "url": "/hue-collective/static/media/adam2.ba4b88c8.png"
+  },
+  {
+    "revision": "b37bbeded37b89a36959958e5c5265f7",
+    "url": "/hue-collective/static/media/adam3.b37bbede.png"
+  },
+  {
+    "revision": "11b9b551edc51b862f2f446788c29660",
+    "url": "/hue-collective/static/media/alex.11b9b551.png"
+  },
+  {
+    "revision": "2a3d364be2bf1bd03a3b50073516133d",
+    "url": "/hue-collective/static/media/alex.2a3d364b.png"
+  },
+  {
+    "revision": "933f8a731b2463c359a0450a6e64ec05",
+    "url": "/hue-collective/static/media/alex.933f8a73.png"
+  },
+  {
+    "revision": "9b5317fad1e6c36bdd9c0737c2fe4f37",
+    "url": "/hue-collective/static/media/alex2.9b5317fa.png"
+  },
+  {
+    "revision": "a6a28bdf1f4475713c9b83b02a19a079",
+    "url": "/hue-collective/static/media/alex3.a6a28bdf.png"
+  },
+  {
+    "revision": "6e3703af5ce8d0f9494442982a64cda7",
+    "url": "/hue-collective/static/media/amy.6e3703af.png"
+  },
+  {
+    "revision": "82de851a30a65f9a1a577466e45cc3a4",
+    "url": "/hue-collective/static/media/amy.82de851a.png"
+  },
+  {
+    "revision": "e8fa063816e429726d5b1d5fefc23a0a",
+    "url": "/hue-collective/static/media/amy.e8fa0638.png"
+  },
+  {
+    "revision": "9d549c786aebec2e5ce4bbed4d811f0e",
+    "url": "/hue-collective/static/media/amy2.9d549c78.png"
+  },
+  {
+    "revision": "cccccc7da26e2cab3f3c07f51cc0a33a",
+    "url": "/hue-collective/static/media/amy3.cccccc7d.png"
+  },
+  {
+    "revision": "6ac9634e2daea6fccceca572f61357e8",
+    "url": "/hue-collective/static/media/becky.6ac9634e.png"
+  },
+  {
+    "revision": "98a7c9c65a03fa018531760a14be8a40",
+    "url": "/hue-collective/static/media/becky.98a7c9c6.png"
+  },
+  {
+    "revision": "bb2ccddd4baf5241c212053949720ddd",
+    "url": "/hue-collective/static/media/becky.bb2ccddd.png"
+  },
+  {
+    "revision": "0c5ecddaa6ffaaa84584cd9e14799c20",
+    "url": "/hue-collective/static/media/becky2.0c5ecdda.png"
+  },
+  {
+    "revision": "fc97456c4e12e862eb32cb3ec68e6ddc",
+    "url": "/hue-collective/static/media/becky3.fc97456c.png"
+  },
+  {
+    "revision": "281f5428cb740e6ca1ee5f8b7e9d8b4b",
+    "url": "/hue-collective/static/media/braeanne.281f5428.png"
+  },
+  {
+    "revision": "3489eb0333320415f9870505ccddc519",
+    "url": "/hue-collective/static/media/braeanne.3489eb03.png"
+  },
+  {
+    "revision": "e33cc8e7156a9a19239d5fba19ff7cf9",
+    "url": "/hue-collective/static/media/braeanne.e33cc8e7.png"
+  },
+  {
+    "revision": "4cdd06e6022ae29d1d2fe5d2a2bfbd5b",
+    "url": "/hue-collective/static/media/braeanne2.4cdd06e6.png"
+  },
+  {
+    "revision": "2227efb875bc6e3c5d8d0c1e3bd8c625",
+    "url": "/hue-collective/static/media/braeanne3.2227efb8.png"
+  },
+  {
+    "revision": "626c662005bf8800c31d467bb8bad8a6",
+    "url": "/hue-collective/static/media/cursor.626c6620.svg"
+  },
+  {
+    "revision": "09fa1e3c3afc0af175d6cf88b1e7e7fe",
+    "url": "/hue-collective/static/media/hemmy.09fa1e3c.png"
+  },
+  {
+    "revision": "320c6757bb7cba6e3b23bfef9e8d519e",
+    "url": "/hue-collective/static/media/hemmy.320c6757.png"
+  },
+  {
+    "revision": "b54b57b3a0b7e6ce0fca674d16f8881b",
+    "url": "/hue-collective/static/media/hemmy.b54b57b3.png"
+  },
+  {
+    "revision": "7562ffc80d668ad1d92c1ddb1bfaade7",
+    "url": "/hue-collective/static/media/hemmy2.7562ffc8.png"
+  },
+  {
+    "revision": "3a800749c6707788d472a67a1c096798",
+    "url": "/hue-collective/static/media/hemmy3.3a800749.png"
+  },
+  {
+    "revision": "829c94edcb0f7abdd4f35e2cd04fdb45",
+    "url": "/hue-collective/static/media/hue.829c94ed.png"
+  },
+  {
+    "revision": "ed4ba2831466a38a8e946e10dec68955",
+    "url": "/hue-collective/static/media/hue.ed4ba283.png"
+  },
+  {
+    "revision": "944ebb963153b98721cd5c08c1e70bf5",
+    "url": "/hue-collective/static/media/hue2.944ebb96.png"
+  },
+  {
+    "revision": "944ebb963153b98721cd5c08c1e70bf5",
+    "url": "/hue-collective/static/media/hue3.944ebb96.png"
+  },
+  {
+    "revision": "e552f3774d6f5d1a7ee037167584ecdd",
+    "url": "/hue-collective/static/media/huecropped2.e552f377.png"
+  },
+  {
+    "revision": "3d891c5512d645a42c0f0ca31dff6372",
+    "url": "/hue-collective/static/media/justin.3d891c55.png"
+  },
+  {
+    "revision": "7c40a5c604127408cbb794b078030804",
+    "url": "/hue-collective/static/media/justin.7c40a5c6.png"
+  },
+  {
+    "revision": "e3aaf8c0710f999c90f97ffbe2e9f0ea",
+    "url": "/hue-collective/static/media/justin.e3aaf8c0.png"
+  },
+  {
+    "revision": "d71513b059308738e321f4c7186df852",
+    "url": "/hue-collective/static/media/justin2.d71513b0.png"
+  },
+  {
+    "revision": "e1baeb0802c110a5f174b24e570edb2a",
+    "url": "/hue-collective/static/media/justin3.e1baeb08.png"
+  },
+  {
+    "revision": "4a4870cad2d0ee6c636166beafa65392",
+    "url": "/hue-collective/static/media/kenny.4a4870ca.png"
+  },
+  {
+    "revision": "6eccfa733e23497753752b2fa5f3921f",
+    "url": "/hue-collective/static/media/kenny.6eccfa73.png"
+  },
+  {
+    "revision": "f7f3b827c07727d5fe616963ee342311",
+    "url": "/hue-collective/static/media/kenny.f7f3b827.png"
+  },
+  {
+    "revision": "48e7aed8e32172d40dc9fb79e8ff5262",
+    "url": "/hue-collective/static/media/kenny2.48e7aed8.png"
+  },
+  {
+    "revision": "c7f64a4a8024ff7312a75ca7fbacc211",
+    "url": "/hue-collective/static/media/kenny3.c7f64a4a.png"
+  },
+  {
+    "revision": "4759132fa0e165938c86532122a9aa29",
+    "url": "/hue-collective/static/media/kirby.4759132f.png"
+  },
+  {
+    "revision": "48b29bd2712af7205b60785f8144327f",
+    "url": "/hue-collective/static/media/kirby.48b29bd2.png"
+  },
+  {
+    "revision": "7e2abafb31dd4d7100ab636c08193e5f",
+    "url": "/hue-collective/static/media/kirby.7e2abafb.png"
+  },
+  {
+    "revision": "4c7886515b904f778cb392d3118c199a",
+    "url": "/hue-collective/static/media/kirby2.4c788651.png"
+  },
+  {
+    "revision": "9662f48b25fc5e6f3a09eab4136eac00",
+    "url": "/hue-collective/static/media/kirby3.9662f48b.png"
+  },
+  {
+    "revision": "62091550217200218da5c73b4ea566df",
+    "url": "/hue-collective/static/media/lisa.62091550.png"
+  },
+  {
+    "revision": "9cc88686c5e8bdff7041f09a30d85498",
+    "url": "/hue-collective/static/media/lisa.9cc88686.png"
+  },
+  {
+    "revision": "abbfddec693d33f941521bf9947c9fad",
+    "url": "/hue-collective/static/media/lisa.abbfddec.png"
+  },
+  {
+    "revision": "db0b6187bb8d2ab3cc1d98b982f3c378",
+    "url": "/hue-collective/static/media/lisa2.db0b6187.png"
+  },
+  {
+    "revision": "4c0b122c13b6312edcc6bf65f8bda3ce",
+    "url": "/hue-collective/static/media/lisa3.4c0b122c.png"
+  },
+  {
+    "revision": "7767aa511e08220e561e0f6165a95825",
+    "url": "/hue-collective/static/media/loader_transparent2.7767aa51.gif"
+  },
+  {
+    "revision": "176a9c26b5267f475bd301cfb296c99d",
+    "url": "/hue-collective/static/media/logo.176a9c26.png"
+  },
+  {
+    "revision": "c03ff4848149e56e9c74ed73a77ef70d",
+    "url": "/hue-collective/static/media/lwin.c03ff484.png"
+  },
+  {
+    "revision": "c279ca15e78456ac70fda76e04505b6a",
+    "url": "/hue-collective/static/media/lwin.c279ca15.png"
+  },
+  {
+    "revision": "cb2d3baa06a978811ce983c2f2fdf196",
+    "url": "/hue-collective/static/media/lwin.cb2d3baa.png"
+  },
+  {
+    "revision": "e3fec23555563064fa51935fd250567e",
+    "url": "/hue-collective/static/media/lwin2.e3fec235.png"
+  },
+  {
+    "revision": "5da2c13b8b433c2bbd728dd3eed90dbb",
+    "url": "/hue-collective/static/media/lwin3.5da2c13b.png"
+  },
+  {
+    "revision": "301c324f44bdca74d1ba948a407a0d7b",
+    "url": "/hue-collective/static/media/mylinh.301c324f.png"
+  },
+  {
+    "revision": "ba3471789d5742aa53ec8ebb8ba4fac7",
+    "url": "/hue-collective/static/media/mylinh.ba347178.png"
+  },
+  {
+    "revision": "e6045a12a49e0f294630e85dc3919959",
+    "url": "/hue-collective/static/media/mylinh.e6045a12.png"
+  },
+  {
+    "revision": "2c480092169a77aaf31f3d92abaca7e2",
+    "url": "/hue-collective/static/media/mylinh2.2c480092.png"
+  },
+  {
+    "revision": "a84aadf17da14fe61724e079b92d8658",
+    "url": "/hue-collective/static/media/mylinh3.a84aadf1.png"
+  },
+  {
+    "revision": "bf7baa37335f6b1b5734bb98e23150c3",
+    "url": "/hue-collective/static/media/promo.bf7baa37.mp4"
+  },
+  {
+    "revision": "2e61af1e8e665ce93c0271e923814c7f",
+    "url": "/hue-collective/static/media/quylan.2e61af1e.png"
+  },
+  {
+    "revision": "464506756028aac4e4ee4db6f312d39e",
+    "url": "/hue-collective/static/media/quylan.46450675.png"
+  },
+  {
+    "revision": "aa79264a03836867d98489ba857630e6",
+    "url": "/hue-collective/static/media/quylan.aa79264a.png"
+  },
+  {
+    "revision": "0ebc8b62b8cd66815b90512f3437aec6",
+    "url": "/hue-collective/static/media/quylan2.0ebc8b62.png"
+  },
+  {
+    "revision": "7f6e017ba33ab3d66008a7a97392696c",
+    "url": "/hue-collective/static/media/quylan3.7f6e017b.png"
+  },
+  {
+    "revision": "6dc149f0b2ee3b4183ad63b91a90d64c",
+    "url": "/hue-collective/static/media/ryan.6dc149f0.png"
+  },
+  {
+    "revision": "98aeca0eab4913080bf2669e47490091",
+    "url": "/hue-collective/static/media/ryan.98aeca0e.png"
+  },
+  {
+    "revision": "d2e30416db56172af8776adf3bb02214",
+    "url": "/hue-collective/static/media/ryan.d2e30416.png"
+  },
+  {
+    "revision": "99bb58b95fd75e8f0fd7c6f54c381fa2",
+    "url": "/hue-collective/static/media/ryan2.99bb58b9.png"
+  },
+  {
+    "revision": "337da963aceefc35a16600103c3c3f00",
+    "url": "/hue-collective/static/media/ryan3.337da963.png"
+  },
+  {
+    "revision": "516a4c75234e9f6488f843b2fc9cf7b7",
+    "url": "/hue-collective/static/media/samantha.516a4c75.png"
+  },
+  {
+    "revision": "cf5c1ddc0f966436906e846f296926c4",
+    "url": "/hue-collective/static/media/samantha.cf5c1ddc.png"
+  },
+  {
+    "revision": "f0a4188b2c5217783fca293ae1011991",
+    "url": "/hue-collective/static/media/samantha.f0a4188b.png"
+  },
+  {
+    "revision": "498a8a55038b0c881dace36bc803633b",
+    "url": "/hue-collective/static/media/samantha2.498a8a55.png"
+  },
+  {
+    "revision": "437ab1b52111a24d3b3c316b396ed1ee",
+    "url": "/hue-collective/static/media/samantha3.437ab1b5.png"
+  },
+  {
+    "revision": "422f578473233c6109fb9d32777e4ab9",
+    "url": "/hue-collective/static/media/tanish.422f5784.png"
+  },
+  {
+    "revision": "d664b3564789a47f381e9cfe89460e2b",
+    "url": "/hue-collective/static/media/tanish.d664b356.png"
+  },
+  {
+    "revision": "e8cb1f0781162c0a43e2a2fb64832684",
+    "url": "/hue-collective/static/media/tanish.e8cb1f07.png"
+  },
+  {
+    "revision": "97083dba5e5c588cd6fe8c23f37de43c",
+    "url": "/hue-collective/static/media/tanish2.97083dba.png"
+  },
+  {
+    "revision": "1c56caa7a8ea49ebb893fc8515d24a03",
+    "url": "/hue-collective/static/media/tanish3.1c56caa7.png"
+  },
+  {
+    "revision": "ac7d958705a56ac1b0ba6d3fc7d239ca",
+    "url": "/hue-collective/static/media/thumbnail.ac7d9587.png"
+  },
+  {
+    "revision": "461ee72e135183226cf9e1b5ca5baeec",
+    "url": "/hue-collective/static/media/white Liza_Doodles.461ee72e.png"
+  },
+  {
+    "revision": "3c672d44f169552317b1332423cab575",
+    "url": "/hue-collective/static/media/white Ryan illustration.3c672d44.png"
+  },
+  {
+    "revision": "5ad57fb29206e4f6558b58a1a6a2b941",
+    "url": "/hue-collective/static/media/white Ryan_Doodles.5ad57fb2.png"
+  },
+  {
+    "revision": "dd8007c7bda2ae292ebbb610905e5518",
+    "url": "/hue-collective/static/media/white Tanish_Doodles.dd8007c7.png"
+  },
+  {
+    "revision": "944ebb963153b98721cd5c08c1e70bf5",
+    "url": "/hue-collective/static/media/white lisa illustration.944ebb96.png"
+  },
+  {
+    "revision": "f088fe2860693d8daba53be5b522c626",
+    "url": "/hue-collective/static/media/white tanish illustration.f088fe28.png"
+  },
+  {
+    "revision": "4c15c605cacfa7673fa0d242d9d7c9ae",
+    "url": "/hue-collective/static/media/zach.4c15c605.png"
+  },
+  {
+    "revision": "754acdb7c0d48ecc2aef402e12f80d4e",
+    "url": "/hue-collective/static/media/zach.754acdb7.png"
+  },
+  {
+    "revision": "8596e02e7ce584c97afcffd0200bd2ec",
+    "url": "/hue-collective/static/media/zach.8596e02e.png"
+  },
+  {
+    "revision": "2538341cde68f2d527d5a49b6e84f44a",
+    "url": "/hue-collective/static/media/zach2.2538341c.png"
+  },
+  {
+    "revision": "d60f20d0ec51248d15406ec912084ca2",
+    "url": "/hue-collective/static/media/zach3.d60f20d0.png"
+  }
+]);
