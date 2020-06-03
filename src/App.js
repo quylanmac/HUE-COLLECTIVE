50dd40c1e62d02e8766654367ddfc104c0eb15a7
Quy Mac <quylanmac@gmail.com>
2020-06-03 15:55:19 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
 import {Test} from './Components/Test';
-
+import {SP20} from './Components/SP20';
 function App() {
   return (
-   <Test></Test>
+   //<Test></Test>
+   <SP20/>
   );
 }
 
