e4be06975d9c26891ffca40236b1d3a0b9c41024
Quy Mac <quylanmac@gmail.com>
2020-06-03 15:55:19 -0700

    test

---
+++ src/Components/SP20.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+export class SP20 extends Component {
+    constructor(props, context) {
+        super(props, context);
+    }
+
+    render() {
+        return (
+            <div>
+                <p>tset</p>
+            </div>
+        )
+    }
+}
+export default SP20;
