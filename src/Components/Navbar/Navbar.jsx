7c2e5088361c471d08084494c4c653e4cd50922f
Quy Mac <quylanmac@gmail.com>
2020-10-02 18:24:06 -0700

    buttons, home page

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,17 +1,18 @@
 import React, { Component, useState, useEffect } from 'react';
 import NavBar from 'react-bootstrap/Navbar';
 import { NavDropdown, Nav, Image, DropdownButton } from 'react-bootstrap';
-import { Redirect } from 'react-router-dom';
+import { Redirect, Link } from 'react-router-dom';
 import { rootState } from '../../redux/reducers';
 import { connect } from 'react-redux';
 import './Navbar.css';
 import BlackLogo from '../../img/SP20/BlackLogo.png';
 import WhiteLogo from '../../img/SP20/WhiteLogo.png';
+import { HashLink } from 'react-router-hash-link';
 const mapStateToProps = (state) => {
     return { color: state.color, theme: state.theme };
 }
 const Navbar = ({ color, theme }) => {
-     const [redirect, redirectTo] = useState(null);
+    const [redirect, redirectTo] = useState(null);
     const [myColor, setColor] = useState("white");
     const [myTheme, setTheme] = useState("black");
     useEffect(() => {
@@ -20,10 +21,9 @@ const Navbar = ({ color, theme }) => {
                 setColor(color.content);
             }
         }
-        console.log(theme);
         if (theme != undefined) {
             if (theme.content != undefined) {
-            setTheme(theme.content);
+                setTheme(theme.content);
             }
         }
         if (redirect != null) {
@@ -31,12 +31,12 @@ const Navbar = ({ color, theme }) => {
         }
     }, [theme]);
 
-   
+
     return (
         <div>
-            {redirect && <Redirect to={redirect} /> }
-           
-            <NavBar className={myTheme === "white" ? 'myNav2': (myTheme==="transparent") ? 'myNav3': (myTheme==="hidden") ? 'myNav4' : 'myNav'} expand="lg">
+            {redirect && <Redirect to={redirect} />}
+
+            <NavBar className={myTheme === "white" ? 'myNav2' : (myTheme === "transparent") ? 'myNav3' : (myTheme === "hidden") ? 'myNav4' : 'myNav'} expand="lg">
                 <NavBar.Brand className="navBrand" onClick={() => redirectTo('/')}>
                     {/* <div className="overlay">
                         <div className="overlay-2">
@@ -46,26 +46,26 @@ const Navbar = ({ color, theme }) => {
                             <p className="mainLogo" style={{ color: myColor }}>HUE</p>
                         </div>
                     </div> */}
-                    {myTheme === "white" ? <img src={BlackLogo} className="imgTest"/> : <img src={WhiteLogo} className="imgTest"/>}
+                    {myTheme === "white" ? <img src={BlackLogo} className="imgTest" /> : <img src={WhiteLogo} className="imgTest" />}
 
                 </NavBar.Brand>
                 <NavBar.Toggle aria-controls="responsive-navbar-nav" />
                 <NavBar.Collapse id="responsive-navbar-nav">
-                <Nav className="ml-auto">
-                    <Nav.Link className={myTheme=== "white" ? 'navLinks2': 'navLinks'} onClick={() => redirectTo('/issues')}>
-                        issues
+                    <Nav className="ml-auto">
+                        <HashLink className={myTheme === "white" ? 'navLinks2 nav-link' : 'navLinks nav-link'} smooth to="/#issueHome" >
+                            issues
+            </HashLink>
+                        <Nav.Link className={myTheme === "white" ? 'navLinks2' : 'navLinks'} onClick={() => redirectTo('/about')}>
+                            about
                     </Nav.Link>
-                    <Nav.Link className={myTheme=== "white" ? 'navLinks2': 'navLinks'} onClick={() => redirectTo('/about')}>
-                        about
-                    </Nav.Link>
-                    <Nav.Link className={myTheme=== "white" ? 'navLinks2': 'navLinks'} onClick={() => redirectTo('/contact')}>
-                        contact
+                        <Nav.Link className={myTheme === "white" ? 'navLinks2' : 'navLinks'} onClick={() => redirectTo('/contact')}>
+                            contact
                     </Nav.Link>
 
-                </Nav>
+                    </Nav>
                 </NavBar.Collapse>
             </NavBar>
-            
+
         </div>
     )
 
