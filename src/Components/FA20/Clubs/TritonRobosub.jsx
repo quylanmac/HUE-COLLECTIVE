c7738ddef46201c00692e246640e5014274fe68a
Quy Mac <quylanmac@gmail.com>
2020-10-06 14:38:18 -0700

    test?

--- src/Components/FA20/Clubs/TritonRobosub.jsx
+++ src/Components/FA20/Clubs/TritonRobosub.jsx
@@ -5,7 +5,8 @@ import '../FA20.css';
 import { setTheme } from '../../../redux/actions';
 import Directory from '../Directory.jsx';
 import './TritonRobosub.css'
-
+import { HashLink } from 'react-router-hash-link';
+import groupButton from '../../../img/Issue2/button.png';
 class TritonRobosub extends Component{
     constructor(props) {
         super(props);
@@ -85,6 +86,9 @@ class TritonRobosub extends Component{
                 </Col>
 
                 </Row>
+                <HashLink smooth to="/issue2#section2" className="end2Button">
+                    <img src={groupButton} />
+                </HashLink>
                 </Container>
         )
     }
