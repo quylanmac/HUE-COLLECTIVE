12a4b39df8d4aaad534037c7469dd1c4b0dacea0
Quy Mac <quylanmac@gmail.com>
2020-10-08 10:51:03 -0700

    test

--- src/Components/FA20/FA20.jsx
+++ src/Components/FA20/FA20.jsx
@@ -34,84 +34,76 @@ class FA20 extends Component {
         this.props.setTheme(theme);
     }
     render() {
-        const parent = { width: `100%`, height: `100%` }
-        const child = { width: `1110px`, height: `100%` }
         return (
             <Container className="container-fluid">
-                <div style={parent} className="row nopad">
-                    <div className="">
-                        <div className="viewResponsive sec1" id="section1">
-                            <Row className="nopad">
-                                <Col md={4}>
-                                    <Link to={{pathname: "/issue2"}}>
-                                    <Row md={2}>
-                                        <Col md={12} className="issue_2">
-                                            <p>ISSUE 2</p>
-                                            <p>PLUGGED IN</p>
-                                        </Col>
-                                    </Row>
-                                    </Link>
-                                    <Row xs={10} md={10}>
-                                        <Col xs={4} md={4}>
-                                            <Row>
-                                                <img className="plug_1" src={plug_1} />
-
-                                                <img className="outlet" src={outlet}/>
-                                            </Row>
-                                        </Col>
-
-                                        <Col xs={8} md={8}>
-                                            <Row md={9}>
-                                                <Directory />
-                                            </Row>
-                                            <Row md={3}>
-                                                <Col md={12} className="design_outlet">
-                                                    <p>Finding your design outlet at UCSD</p>
-                                                </Col>
-                                            </Row>
-                                        </Col>
-
-
-                                    </Row>
-                                </Col>
-
-                                {/* COL 2 */}
-                                <Col md={3}>
-                                    <Row md={9}>
-                                        <img className="clouds" src={require('./Clouds.svg')} />
-                                    </Row>
-                                    <Row md={3}>
-                                        <img className="smiley" src={require('./Smiley.svg')} />
-                                    </Row>
-                                </Col>
-
-                                {/* COL 3 */}
-                                <Col md={5}>
-                                    <Row md={3}>
-                                        <img className="arrows" src={require('./Arrows.svg')} />
-                                    </Row>
-                                    <Row md={2}>
-                                        <img className="text" src={require('./Text.svg')} />
-                                    </Row>
-                                    <Row md={5}>
-                                        <img className="mug" src={require('./Mug.svg')} />
-                                    </Row>
-                                    <Row className="title" md={2}>
-                                        <p>HUECOLLECTIVE.CO</p>
-                                    </Row>
+                <Row className="nopad">
+                    <Col md={4}>
+                        <Link to={{ pathname: "/issue2" }}>
+                            <Row md={2}>
+                                <Col md={12} className="issue_2">
+                                    <p>ISSUE 2</p>
+                                    <p>PLUGGED IN</p>
                                 </Col>
                             </Row>
+                        </Link>
+                        <Row xs={10} md={10}>
+                            <Col xs={4} md={4}>
+                                <Row>
+                                    <img className="plug_1" src={plug_1} />
+
+                                    <img className="outlet" src={outlet} />
+                                </Row>
+                            </Col>
+
+                            <Col xs={8} md={8}>
+                                <Row md={9}>
+                                    <Directory />
+                                </Row>
+                                <Row md={3}>
+                                    <Col md={12} className="design_outlet">
+                                        <p>Finding your design outlet at UCSD</p>
+                                    </Col>
+                                </Row>
+                            </Col>
 
 
-                            <Row className="horizontal_scroll justifyEnd">
-                                <HashLink smooth to="/groupinterview" className="">
-                                    <img src={groupButton} className="end2Button responseImg2" />
-                                </HashLink>
-                            </Row>
-                        </div>
+                        </Row>
+                    </Col>
 
-                    </div>
-                </div>
+                    {/* COL 2 */}
+                    <Col md={3}>
+                        <Row md={9}>
+                            <img className="clouds" src={require('./Clouds.svg')} />
+                        </Row>
+                        <Row md={3}>
+                            <img className="smiley" src={require('./Smiley.svg')} />
+                        </Row>
+                    </Col>
+
+                    {/* COL 3 */}
+                    <Col md={5}>
+                        <Row md={3}>
+                            <img className="arrows" src={require('./Arrows.svg')} />
+                        </Row>
+                        <Row md={2}>
+                            <img className="text" src={require('./Text.svg')} />
+                        </Row>
+                        <Row md={5}>
+                            <img className="mug" src={require('./Mug.svg')} />
+                        </Row>
+                        <Row className="title" md={2}>
+                            <p>HUECOLLECTIVE.CO</p>
+                        </Row>
+                    </Col>
+                </Row>
+
+
+                <Row className="horizontal_scroll justifyEnd">
+                    <HashLink smooth to="/groupinterview" className="">
+                        <img src={groupButton} className="end2Button responseImg2" />
+                    </HashLink>
+                </Row>
+
             </Container>
         )
     }
