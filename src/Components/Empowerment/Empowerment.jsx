82ad70d01bcfb04149399f9b1cbb27975b0ec2fe
Quy Mac <quylanmac@gmail.com>
2020-10-14 22:19:47 -0700

    renumbered winson questions

--- src/Components/Empowerment/Empowerment.jsx
+++ src/Components/Empowerment/Empowerment.jsx
@@ -95,7 +95,7 @@ class Empowerment extends Component {
                                 My orientation leader in college actually recommended that I come out to a student organization called Design Co (previously Design at UCSD). I got involved in design not through classes or opportunities but just by talking to people. I’ve met many young talented designers at UCSD who taught me a lot about design and helped me get further involved in the design community at UCSD. In my experience, there are a lot of people who are just willing to have candid conversations on design and how to get involved in UCSD’s design community.
                                 <br></br><br></br>
                                         <b>
-                                            Q6. What have been your favorite design classes at UCSD?</b>
+                                            Q4. What have been your favorite design classes at UCSD?</b>
                                         <br></br><br></br>
 
                                 ENG 100D - Design for Development
@@ -105,13 +105,13 @@ class Empowerment extends Component {
                                 In my opinion, this class distinguishes itself from other classes by being relentlessly practical. It really helped me to find internships within the design industry. I also appreciated the critique on other design classes.
                                 <br></br><br></br>
                                         <b>
-                                            Q7. What valuable skills did you learn that you think every designer should know outside of the classroom?
+                                            Q5. What valuable skills did you learn that you think every designer should know outside of the classroom?
                                 </b><br></br><br></br>
 
                                 Classes were great for setting the foundation for design work but, in order to hone in my skills and learn how to design, I turned to online resources. I read articles about design every single day. There are so many articles on Medium and other online resources that have valuable information on design and the industry. I also connected with designers online and in person which was really impactful for me. I highly recommend having a group of designers to talk to. As students, we may not know how to start since there are so many different paths to take within design. Talking with friends allowed us to explore these paths together, asking each other questions and learning from each other.
                                 <br></br><br></br>
                                         <b>
-                                            Q8. How would someone get involved with a design project/get experience outside of class?</b>
+                                            Q6. How would someone get involved with a design project/get experience outside of class?</b>
                                         <br></br><br></br>
                                 An easy way to get involved would be to join a student group. Personally, joining Design Co has made me more comfortable and confident in getting involved with side projects. They host weekly general body meetings that include beginner friendly workshops and industry talks.
                                 </p>
